b6b5a3bd26fc937ace8c041dc51598e3bb7547de
am813nt <11208844+am813nt@users.noreply.github.com>
2017-11-20 20:13:06 +0500

    Fixed license and style

--- fs_ro.c
+++ fs_ro.c
@@ -1,76 +1,27 @@
-/*
-** Zabbix
-** Copyright (C) 2001-2016 Zabbix SIA
-**
-** This program is free software; you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 2 of the License, or
-** (at your option) any later version.
-**
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-** GNU General Public License for more details.
-**
-** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software
-** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-**/
-
 #include "sysinc.h"
 #include "module.h"
 #include <mntent.h>
 
-/* the variable keeps timeout setting for item processing */
 static int      item_timeout = 0;
 
 int     zbx_module_fs_ro_status(AGENT_REQUEST *request, AGENT_RESULT *result);
 
 static ZBX_METRIC keys[] =
-/*      KEY                     FLAG            FUNCTION                TEST PARAMETERS */
 {
         {"fs_ro.status",        0,              zbx_module_fs_ro_status,        NULL},
         {NULL}
 };
 
-/******************************************************************************
- *                                                                            *
- * Function: zbx_module_api_version                                           *
- *                                                                            *
- * Purpose: returns version number of the module interface                    *
- *                                                                            *
- * Return value: ZBX_MODULE_API_VERSION_ONE - the only version supported by   *
- *               Zabbix currently                                             *
- *                                                                            *
- ******************************************************************************/
 int     zbx_module_api_version()
 {
         return ZBX_MODULE_API_VERSION_ONE;
 }
 
-/******************************************************************************
- *                                                                            *
- * Function: zbx_module_item_timeout                                          *
- *                                                                            *
- * Purpose: set timeout value for processing of items                         *
- *                                                                            *
- * Parameters: timeout - timeout in seconds, 0 - no timeout set               *
- *                                                                            *
- ******************************************************************************/
 void    zbx_module_item_timeout(int timeout)
 {
         item_timeout = timeout;
 }
 
-/******************************************************************************
- *                                                                            *
- * Function: zbx_module_item_list                                             *
- *                                                                            *
- * Purpose: returns list of item keys supported by the module                 *
- *                                                                            *
- * Return value: list of item keys                                            *
- *                                                                            *
- ******************************************************************************/
 ZBX_METRIC      *zbx_module_item_list()
 {
         return keys;
@@ -96,36 +47,11 @@ int     zbx_module_fs_ro_status(AGENT_REQUEST *request, AGENT_RESULT *result)
         return SYSINFO_RET_OK;
 }
 
-
-/******************************************************************************
- *                                                                            *
- * Function: zbx_module_init                                                  *
- *                                                                            *
- * Purpose: the function is called on agent startup                           *
- *          It should be used to call any initialization routines             *
- *                                                                            *
- * Return value: ZBX_MODULE_OK - success                                      *
- *               ZBX_MODULE_FAIL - module initialization failed               *
- *                                                                            *
- * Comment: the module won't be loaded in case of ZBX_MODULE_FAIL             *
- *                                                                            *
- ******************************************************************************/
 int     zbx_module_init()
 {
         return ZBX_MODULE_OK;
 }
 
-/******************************************************************************
- *                                                                            *
- * Function: zbx_module_uninit                                                *
- *                                                                            *
- * Purpose: the function is called on agent shutdown                          *
- *          It should be used to cleanup used resources if there are any      *
- *                                                                            *
- * Return value: ZBX_MODULE_OK - success                                      *
- *               ZBX_MODULE_FAIL - function failed                            *
- *                                                                            *
- ******************************************************************************/
 int     zbx_module_uninit()
 {
         return ZBX_MODULE_OK;
